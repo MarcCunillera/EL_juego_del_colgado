699b4316124410ae98f2ad877525964876782602
MarcCunillera <marccunillera2006@gmail.com>
2024-11-18 19:58:21 +0100

    Descripcio de les normes

---
+++ src/paquete1/El_juego_del_colgado.java
@@ -0,0 +1,26 @@
+package paquete1;
+
+public class El_juego_del_colgado {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("**************************");
+		System.out.println("   El Juego del colgado   ");
+		System.out.println("**************************");
+		
+		System.out.print("Reglas del juego:\r\n"
+				+ "- El juego comienza pidiendo al administrador (un jugador) que elija una palabra secreta, la cual debe ser oculta durante el juego.\r\n"
+				+ "- Cada jugador tiene que adivinar una letra de la palabra secreta en cada ronda.\r\n"
+				+ "- El número máximo de intentos por jugador es 6.\r\n"
+				+ "- Si un jugador adivina correctamente una letra, esta se revela en su posición en la palabra secreta.\r\n"
+				+ "- Si un jugador se queda sin intentos, pierde la ronda.\r\n"
+				+ "- El jugador que adivine toda la palabra primero o quien gane más rondas será el ganador.");
+		
+		String[] paraules = {"taula, cadira, ordenador, llapis"};
+		
+		System.out.println();//
+	}
+
+}
